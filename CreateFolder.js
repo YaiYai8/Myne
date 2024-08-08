7393a9b4e3e9771b5d407944a6fd4a07dae920f0
YaiYai8 <164002601+YaiYai8@users.noreply.github.com>
2024-08-08 12:49:00 +0300

    Create CreateFolder.js

---
+++ CreateFolder.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const code = `
+const dirPath = path.join('C:', 'Temp', 'YAIR');
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+}
+`;
+
+eval(code);
